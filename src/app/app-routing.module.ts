0dfe92d5b948917e1d3ff9ad3687dec2db9c476e
Alexis Largaiolli <alexis.largaiolli@agicap.com>
2023-11-13 20:04:43 +0100

    feat: add page component and basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {authenticatedGuard} from "./guards/authenticated.guard";
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  loadComponent: () => import('./components/welcome/welcome.component').then(m => m.WelcomeComponent),
+  canActivate: [authenticatedGuard],
+}, {
+  path: 'sign-in',
+  loadComponent: () => import('./components/sign-in/sign-in.component').then(m => m.SignInComponent),
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/sign-in/sign-in.component.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-sign-in',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SignInComponent {
+
+}
