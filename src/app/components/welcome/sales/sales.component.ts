0c4bca454683a34b9146f6f86b51b766e8afda48
Alexis Largaiolli <alexis.largaiolli@agicap.com>
2023-11-14 06:39:48 +0100

    feat: add product search bar

--- src/app/components/welcome/sales/sales.component.ts
+++ src/app/components/welcome/sales/sales.component.ts
@@ -1,12 +1,19 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  Signal,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableModule } from 'primeng/table';
 import { ProductsService } from '../../../services/products.service';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-sales',
   standalone: true,
-  imports: [CommonModule, TableModule],
+  imports: [CommonModule, TableModule, InputTextModule, ButtonModule],
   templateUrl: './sales.component.html',
   styleUrls: ['./sales.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -14,8 +21,14 @@ import { ProductsService } from '../../../services/products.service';
 export class SalesComponent {
   products = this.productsService.products;
   columns = this.productsService.columns;
+  searchableColumns: Signal<string[]> = computed(() =>
+    this.columns()
+      .filter((column) => column.field)
+      .map((column) => column.field as string),
+  );
 
   constructor(private productsService: ProductsService) {}
 
   protected readonly Number = Number;
+  protected readonly HTMLInputElement = HTMLInputElement;
 }
