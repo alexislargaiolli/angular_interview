f1038ca7daad0c34a5351c24ae80bfbea65ebd6c
Alexis Largaiolli <alexis.largaiolli@agicap.com>
2023-11-14 09:26:55 +0100

    feat: improve sign in validation

--- src/app/components/welcome/sales/sales.component.ts
+++ src/app/components/welcome/sales/sales.component.ts
@@ -10,11 +10,13 @@ import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sales',
   standalone: true,
   imports: [
+    CommonModule,
     TableModule,
     InputTextModule,
     ButtonModule,
