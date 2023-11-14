125ec9e32e7774d250c2847fa8402c0eeb409b97
Alexis Largaiolli <alexis.largaiolli@agicap.com>
2023-11-14 10:07:17 +0100

    feat: add test on products-api.service.spec.ts

--- src/app/services/products-api.service.spec.ts
+++ src/app/services/products-api.service.spec.ts
@@ -1,16 +1,33 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ProductsApiService } from './products-api.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 describe('ProductsApiService', () => {
   let service: ProductsApiService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(ProductsApiService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('getProducts', () => {
+    it('should return the content of potato_sales.json file', () => {
+      service.getProducts().subscribe();
+      const req = httpTestingController.expectOne('/assets/potato_sales.json');
+      req.flush({});
+    });
+  });
+
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
   });
 });
