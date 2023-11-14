6b4377e0f566cbc1e5e8e595e635ce8ffe896e05
Alexis Largaiolli <alexis.largaiolli@agicap.com>
2023-11-14 10:35:25 +0100

    feat: add test to sales.component.spec.ts

--- src/app/components/welcome/sales/sales.component.spec.ts
+++ src/app/components/welcome/sales/sales.component.spec.ts
@@ -1,21 +1,73 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SalesComponent } from './sales.component';
+import {
+  createComponentFactory,
+  mockProvider,
+  Spectator,
+} from '@ngneat/spectator';
+import { ProductsService } from '../../../services/products.service';
+import { signal } from '@angular/core';
 
 describe('SalesComponent', () => {
-  let component: SalesComponent;
-  let fixture: ComponentFixture<SalesComponent>;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [SalesComponent]
-    });
-    fixture = TestBed.createComponent(SalesComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  let spectator: Spectator<SalesComponent>;
+  const columns = [
+    { header: 'Product ID', field: 'productID' },
+    { header: 'Product name', field: 'productName' },
+    {
+      header: 'Sales',
+      subHeaders: [
+        { header: '2022Q1', field: 'salesQ1' },
+        { header: '2022Q2', field: 'salesQ2' },
+        { header: '2022Q3', field: 'salesQ3' },
+        { header: '2022Q4', field: 'salesQ4' },
+      ],
+    },
+    { header: 'Total sales', field: 'totalSales' },
+  ];
+  const products = [
+    {
+      productID: '1',
+      productName: 'Yukon Gold Potatos',
+      salesQ1: 1,
+      salesQ2: 1,
+      salesQ3: 1,
+      salesQ4: 1,
+      totalSales: 4,
+    },
+  ];
+  const createComponent = createComponentFactory({
+    component: SalesComponent,
+    providers: [
+      mockProvider(ProductsService, {
+        products: signal(products),
+        columns: signal(columns),
+      }),
+    ],
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  beforeEach(() => {
+    spectator = createComponent();
+  });
+
+  it('should display a table of product', () => {
+    expect(spectator.queryAll('tr th')).toHaveText([
+      'Product ID',
+      'Product name',
+      'Sales',
+      'Total sales',
+      '2022Q1',
+      '2022Q2',
+      '2022Q3',
+      '2022Q4',
+    ]);
+    expect(spectator.queryAll('tr td')).toHaveText([
+      '1',
+      'Yukon Gold Potatos',
+      '1',
+      '1',
+      '1',
+      '1',
+      '4',
+      '',
+    ]);
   });
 });
--- src/app/components/welcome/welcome.component.spec.ts
+++ src/app/components/welcome/welcome.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WelcomeComponent } from './welcome.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('WelcomeComponent', () => {
   let component: WelcomeComponent;
@@ -8,7 +9,7 @@ describe('WelcomeComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [WelcomeComponent],
+      imports: [WelcomeComponent, RouterTestingModule],
     });
     fixture = TestBed.createComponent(WelcomeComponent);
     component = fixture.componentInstance;
