eb109b5268972d8e268b205400737c13750b7e9b
Alexis Largaiolli <alexis.largaiolli@agicap.com>
2023-11-14 10:12:07 +0100

    feat: add test on auth.service.spec.ts

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,16 +1,33 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
+import { Observer } from 'rxjs';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let observer: Observer<unknown>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(AuthService);
+    observer = {
+      next: jasmine.createSpy(),
+      error: jasmine.createSpy(),
+      complete: jasmine.createSpy(),
+    };
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('login()', () => {
+    it('should authenticate when using username:password credentials', () => {
+      service.authenticated$.subscribe(observer);
+      service.login('username', 'password');
+      expect(observer.next).toHaveBeenCalledWith(true);
+    });
+
+    it('should not authenticate when using other credentials than username:password', () => {
+      service.authenticated$.subscribe(observer);
+      service.login('username@gmail.com', 'azerty');
+      expect(observer.next).toHaveBeenCalledWith(false);
+    });
   });
 });
