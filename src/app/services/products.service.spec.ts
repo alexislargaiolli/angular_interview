da2862638b70bc70f12702ad30db085de3d211b3
Alexis Largaiolli <alexis.largaiolli@agicap.com>
2023-11-14 10:23:36 +0100

    feat: add test on products.service.spec.ts

--- src/app/services/products.service.spec.ts
+++ src/app/services/products.service.spec.ts
@@ -1,16 +1,93 @@
-import { TestBed } from '@angular/core/testing';
-
 import { ProductsService } from './products.service';
+import {
+  createServiceFactory,
+  mockProvider,
+  SpectatorService,
+} from '@ngneat/spectator';
+import { ProductsApiService } from './products-api.service';
+import { of } from 'rxjs';
 
 describe('ProductsService', () => {
-  let service: ProductsService;
+  let spectator: SpectatorService<ProductsService>;
+  const createService = createServiceFactory({
+    service: ProductsService,
+    providers: [mockProvider(ProductsApiService)],
+  });
+  const data = {
+    column: [
+      { header: 'Product ID', field: 'productID' },
+      { header: 'Product name', field: 'productName' },
+      {
+        header: 'Sales',
+        subHeaders: [
+          { header: '2022Q1', field: 'salesQ1' },
+          { header: '2022Q2', field: 'salesQ2' },
+          { header: '2022Q3', field: 'salesQ3' },
+          { header: '2022Q4', field: 'salesQ4' },
+        ],
+      },
+      { header: 'Total sales' },
+    ],
+    data: [
+      {
+        productID: '5068764589210',
+        productName: 'Yukon Gold Potatos',
+        salesQ1: 1,
+        salesQ2: 1,
+        salesQ3: 1,
+        salesQ4: 1,
+      },
+    ],
+  };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ProductsService);
+    spectator = createService();
+    spectator.inject(ProductsApiService).getProducts.andReturn(of(data));
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('loadProducts', () => {
+    beforeEach(() => {
+      spectator.service.loadProducts();
+    });
+
+    it('should call loadProduct from api service', () => {
+      expect(
+        spectator.inject(ProductsApiService).getProducts,
+      ).toHaveBeenCalled();
+    });
+
+    it('should init products', () => {
+      expect(spectator.service.products().length).toBeGreaterThan(0);
+    });
+
+    it('should init columns', () => {
+      expect(spectator.service.columns().length).toBeGreaterThan(0);
+    });
+  });
+
+  describe('products', () => {
+    beforeEach(() => {
+      spectator.service.loadProducts();
+    });
+
+    it('should compute total sales', () => {
+      expect(spectator.service.products()[0].totalSales).toEqual(4);
+    });
+  });
+
+  describe('columns', () => {
+    beforeEach(() => {
+      spectator.service.loadProducts();
+    });
+
+    it('should add totalSales field', () => {
+      const totalSalesColumn = spectator.service
+        .columns()
+        .find((column) => column.header === 'Total sales');
+      expect(totalSalesColumn).toEqual({
+        header: 'Total sales',
+        field: 'totalSales',
+      });
+    });
   });
 });
